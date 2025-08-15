4e5361c3cb1f071a25d04e1031cbd295e04aa117
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-08-15 17:01:37 +0300

    add check box

--- project2/src/Form.tsx
+++ project2/src/Form.tsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 function Form() {
     const [formData, setFormData] = useState({
         name: "",
-        email: ""
+        email: "",
+        info: "",
+        isStudent: false,
     });
 
     
@@ -24,6 +26,19 @@ function Form() {
         className="border border-gray-300 p-2 rounded mb-4 w-64"
         placeholder="Your Email"
         />
+      <p>Your Info {formData.info}</p>
+      <label className="mb-2 text-lg font-semibold">Enter additional info:</label>
+      <textarea
+        onChange={(e) => setFormData({ ...formData, info: e.target.value })}
+        className="border border-gray-300 p-2 rounded mb-4 w-64"
+        placeholder="Your Info"
+      />
+      <label className="mb-2 text-lg font-semibold">Are you a student?</label>
+      <input
+        type="checkbox"
+        checked={formData.isStudent}
+        onChange={(e) => setFormData({ ...formData, isStudent: e.target.checked })}
+      />
       <button
         type="submit"
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
