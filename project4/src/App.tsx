cee06bbcf00d00d28d3209f3ff3d9e73d38b6cf0
mohamedalsamrai <muhamed.alsamrai@gmail.com>
2025-08-21 17:23:42 +0300

    Add product listing and detail pages with routing

--- project4/src/App.tsx
+++ project4/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, Outlet, useParams } from 'react-router-dom'
 
 function App() {
 
@@ -7,6 +7,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} /> 
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:id" element={<Product />} />
     </Routes>
   )
 }
@@ -40,4 +42,35 @@ function Contact() {
   return <h1>Contact</h1>
 }
 
-export default App
+function Products() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <button className='bg-blue-500 text-white p-2 rounded-md m-2'>
+        <Link to="/products/1">Product 1</Link>
+      </button>
+      <button className='bg-blue-500 text-white p-2 rounded-md m-2'>
+        <Link to="/products/2">Product 2</Link>  
+      </button>
+      <button className='bg-blue-500 text-white p-2 rounded-md m-2'>
+        <Link to="/products/3">Product 3</Link>
+      </button>
+      <Outlet />
+    </div>
+  )
+}
+
+function Product() {
+  const { id } = useParams()
+  return (
+    <div>
+      <h1>Product {id}</h1>
+      <button className='bg-blue-500 text-white p-2 rounded-md m-2'>
+        <Link to="/products">Back to Products</Link>
+      </button>
+    </div>
+  )
+}
+
+
+  export default App
